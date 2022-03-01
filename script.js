655dd9235e00dacaad9223e9b422ceaa0d99bd15
MDC 219 <mcouch219@gmail.com>
2022-03-01 12:15:45 -0500

    JSBasics

---
+++ script.js
@@ -0,0 +1,50 @@
+7;
+14;
+21;
+28;//
+// The five main data types are: Numbers,Strings,Bouleans,Undefined and Null.//
+
+// Adding and subtracting + is for Additon - is for Subtraction )
+
+//  Multiplying and dividing(* multiplicaton- / is division)
+
+//  Exponentation ( Exponents) **
+
+// Modulus  (remainder)
+// Returns the division remainder
+// Percentage sign "%"" is used for Modulus
+
+//  Order of operation in JS (Parentheses,Exponents,Multiplication, Division, Addition, Subtraction)//
+
+console.log(1%5);//1
+
+console.log(5%1);//0
+
+console.log('I\'m Mary Poppins,Y\'all');//Im Mary Poppins,Yall
+
+console.log("Hawk" + "Eye");//HawkEye
+
+console.log("Michael Couch".length);//13// (Michael.lemgth.+ Couch.length)
+
+console.log("Avengers: Endgame"[16]);//e ///Breakdown(A)
+
+console.log("\\");///
+
+console.log(5 **4)-9;////616 or (5 **4)-9
+
+console.log("I".length + "Am".length % "Iron".length -"Man".length );
+
+console.log("CAP"[1])
+console.log("VISION"[0])
+console.log("OKOYE"[4])
+console.log("NICK FURY"[0])
+console.log("GROOT"[13])
+console.log("DOCTOR STRANGE"[3])
+console.log("STAR-LORD"[0])
+
+console.log("CAP"[1] + "VISION"[0] + "OKOYE"[4] + "NICK FURY"[0])
+
+let m = 25;
+let c = 4;
+
+
